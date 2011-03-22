d7804fea8c22205af2196bf36c037b9d6b44e2f6
tuetschek <tuetschek@gmail.com>
2011-03-22 16:42:05 +0000

    Added memory parameter to control Java heap size.

--- help-scripts/experiment.sh
+++ help-scripts/experiment.sh
@@ -1,25 +1,41 @@
 #!/bin/bash
-
 #
-# pouziti: qsub -cwd -S /bin/bash bin/experiment.sh plan params
-# 
-# plan se uvadi jen nazev souboru v podadresari plan/ bez pripony!
+# This creates a directory for experiments, linking to the input data files,
+# and runs ML-Process with the specified parameters and scenario. The name
+# of the created directory is by default identical to the name of the scenario.
+# If the directory exists, it is not recreated.
+#
+# The ROOT directory (and possibly other directories) must be configured before 
+# use!
+#
+# All scenario files should have the extension .plan and be situated in a
+# plan/ subdirectory.
+# All data files needed should be situated in the data/ subdirectory.
+#
+#
+# Usage: experiment.sh [-r] [-d directory] scenario-name params
+# -d = run in a directory named differently
+# -r = restart the plan (delete the to-do file first)
 
 
-ROOT=/home/odusek/work/dipl
+ROOT=/home/odusek/work/dipl # set main working directory here
 BINNAME=ml-process.jar
 BINDIR=$ROOT/bin
 BIN=$BINDIR/$BINNAME
 DATA=$ROOT/data
-TRAIN=$DATA/train.txt
-DEVEL=$DATA/devel.txt
-EVAL="$DATA/eval.txt"
-CONF="$DATA/st-en.conf"
-CLUSTERS="$DATA/clusters-*.txt"
+LANG=cs # set-up language here
+TRAIN=$DATA/$LANG/train.txt
+DEVEL=$DATA/$LANG/devel.txt
+EVAL="$DATA/$LANG/eval.txt"
+CONF="$DATA/$LANG/st-$LANG.conf"
+CLUSTERS="$DATA/$LANG/clusters-*.txt"
+PREDINFO="$DATA/$LANG/predinfo.txt"
 PLAN=$ROOT/plan
 EXT=".plan"
+MEM=-Xmx12g
 
-ARCH=`uname --all | sed 's/.* \([^ ]*\) [^ ]*$/\1/'`
+# LP_Solve libraries for the right architecture
+ARCH=`uname --all | sed 's/.* \([^ ]*\) [^ ]*$/\1/'` 
 if [[ $ARCH = 'i686' ]]; then
     DLL_PATH=$BINDIR/dll_32
 elif [[ $ARCH == *64* ]]; then
@@ -29,8 +45,9 @@ else
     exit 1;
 fi
 
-while [[ $1 = "-d" || $1 = "-r" ]]; do # compute in a different directory / reset
-    case $1 in
+# compute in a different directory / reset
+while [[ "$1" = "-d" || "$1" = "-r" || "$1" = "-m" ]]; do 
+    case "$1" in
         "-d")
             DIR=$ROOT/"$2"
             shift
@@ -40,11 +57,16 @@ while [[ $1 = "-d" || $1 = "-r" ]]; do # compute in a different directory / rese
             RESET=1
             shift
             ;;
+        "-m")
+            MEM=-Xmx$2
+            shift
+            shift
+            ;;
     esac
 done
 
 if [[ $# -lt 1 ]] || [[ ! -f $PLAN/"$1"$EXT ]]; then
-    echo "Usage: ./experiment.sh [-d work-dir-name][-r] plan-name params";
+    echo "Usage: ./experiment.sh [-d work-dir-name][-r][-m heapsize] plan-name params";
     exit 1;
 fi
 
@@ -72,6 +94,11 @@ if [[ $RESET = 1 ]]; then # remove the plan status if necessary
     rm $FILE.* 2> /dev/null
 fi
 
+mkdir /mnt/h/tmp/mlprocess-temp
+
 shift  #run the process
+export LC_ALL="en_US.UTF-8"
 export LD_LIBRARY_PATH="$DLL_PATH:$LD_LIBRARY_PATH"
-java  -Djava.library.path=$DLL_PATH -Xms128m -Xmx2g -jar $BINNAME "$@" "$FILE"
+java  -Djava.library.path=$DLL_PATH -Xms128m $MEM -jar $BINNAME "$@" "$FILE"
+
+rm -rf /mnt/h/tmp/mlprocess-temp
