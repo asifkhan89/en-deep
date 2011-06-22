5c36393de7a02c99740099d918af298192b99a1e
tuetschek <tuetschek@gmail.com>
2011-06-22 12:19:39 +0000

    enhancements for BigDataSplitter (SimpleDataSplitter is TOO slow)

--- ml-process/src/en_deep/mlprocess/manipulation/BigDataSplitter.java
+++ ml-process/src/en_deep/mlprocess/manipulation/BigDataSplitter.java
@@ -33,17 +33,23 @@ import en_deep.mlprocess.exception.TaskException;
 import en_deep.mlprocess.utils.FileUtils;
 import en_deep.mlprocess.utils.StringUtils;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Vector;
+import weka.core.Attribute;
 import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ArffLoader;
+import weka.core.converters.ArffSaver;
+import weka.core.converters.Saver;
 
 /**
- * This will split a big ARFF file into chunks based on the number of input instances and then convert all string
+ * This will split a big ARFF file into chunks based on the number of input instances and then optionally convert all string
  * headers to nominal.
  * @author Ondrej Dusek
  */
@@ -57,6 +63,10 @@ public class BigDataSplitter extends Task {
     private static final String CHUNKS_NO = "chunks_no";
     /** The 'equal_chunks' parameter name */
     private static final String EQUAL_CHUNKS = "equal_chunks";
+    /** The 'by_attribute' parameter name */
+    private static final String BY_ATTRIBUTE = "by_attribute";
+    /** The 'string_to_nom' parameter name */
+    private static final String STRING_TO_NOM = "string_to_nom";
 
     /* DATA */
 
@@ -68,6 +78,10 @@ public class BigDataSplitter extends Task {
     private int chunksNo;
     /** The header of the original file */
     private Instances header;
+    /** The attribute to be used for splitting */
+    private String splitAttribute;
+    /** Should all string attributes be converted to nominal ? */
+    private boolean stringToNom;
 
     /* METHODS */
 
@@ -75,14 +89,19 @@ public class BigDataSplitter extends Task {
      * This creates a new {@link BigDataSplitter} task, checking the numbers of inputs and outputs
      * and the necessary parameters.
      * <ul>
+     * <li><tt>by_attribute</tt> -- name of the attribute to be used for splitting
      * <li><tt>chunk_length</tt> -- length of one chunk (in number of instances)
      * <li><tt>chunks_no</tt> -- number of (equally large) parts to be produced
      * </ul>
-     * Both parameters are mutually exclusive. There is also a voluntary parameter, if <tt>chunk_length</tt>
+     * All parameters are mutually exclusive. There is also a voluntary parameter, if <tt>chunk_length</tt>
      * is set:
      * <ul>
      * <li><tt>equal_chunks</tt> -- if set, the data will be split into as many chunks as the preset
      * length would cause, but in equal amounts (the last chunk won't be shorter than the previous ones).
+     * </ul>
+     * Another optional parameter:
+     * <ul>
+     * <li><tt>string_to_nom</tt> -- if set, all string attributes will be converted to nominal ones
      * </ul>
      * The number of input files must be equal to the number of output patterns.
      *
@@ -92,10 +111,14 @@ public class BigDataSplitter extends Task {
 
         super(id, parameters, input, output);
 
-        if (!this.hasParameter(CHUNK_LENGTH) && !this.hasParameter(CHUNKS_NO)){
+        if (!this.hasParameter(BY_ATTRIBUTE) && !this.hasParameter(CHUNK_LENGTH) && !this.hasParameter(CHUNKS_NO)){
             throw new TaskException(TaskException.ERR_INVALID_PARAMS, this.id, "Missing parameters.");
         }
-        if (this.hasParameter(CHUNKS_NO)){
+        if (this.hasParameter(BY_ATTRIBUTE)){
+            this.splitAttribute = this.getParameterVal(BY_ATTRIBUTE);
+        }
+        else if(this.hasParameter(CHUNKS_NO))
+        {
             this.chunksNo = this.getIntParameterVal(CHUNKS_NO);
             this.chunkLength = -1;
         }
@@ -125,10 +148,15 @@ public class BigDataSplitter extends Task {
             for (int fileNo = 0; fileNo < this.input.size(); ++fileNo){
 
                 this.loadHeader(fileNo);
-                if (this.chunkLength == -1 || this.getBooleanParameterVal(EQUAL_CHUNKS)){
-                    this.determineChunkLength(fileNo);
+                if (this.splitAttribute != null){
+                    this.splitByAttribute(fileNo);
                 }
-                this.processData(fileNo);
+                else {
+                    if (this.chunkLength == -1 || this.getBooleanParameterVal(EQUAL_CHUNKS)){
+                        this.determineChunkLength(fileNo);
+                    }
+                    this.splitToChunks(fileNo);
+                }
                 this.header = null;
             }
         }
@@ -156,7 +184,7 @@ public class BigDataSplitter extends Task {
      * @param fileNo number of the input file to be processed
      * @throws Exception
      */
-    private void processData(int fileNo) throws Exception {
+    private void splitToChunks(int fileNo) throws Exception {
 
         BufferedReader inRead = openAndSkipHeader(this.input.get(fileNo));
 
@@ -243,6 +271,57 @@ public class BigDataSplitter extends Task {
         Logger.getInstance().message("Found " + numInst + " instances, chunk length "
                 + this.chunkLength + ", greater " + this.greaterChunks, Logger.V_DEBUG);
 
+    }
+
+    private void splitByAttribute(int fileNo) throws TaskException, IOException {
+
+        Attribute splitAttrib;
+
+        // find out all attribute values (end if the attribute does not exist or is not string)
+        if ((splitAttrib = this.header.attribute(this.parameters.get(BY_ATTRIBUTE))) == null
+                || !(splitAttrib.isNominal() || splitAttrib.isString())){
+
+            throw new TaskException(TaskException.ERR_INVALID_PARAMS, this.id, "Attribute "
+                    + this.splitAttribute + " not found or not nominal/string in file " + this.input.get(fileNo));
+        }
+
+        BufferedReader inRead = openAndSkipHeader(this.input.get(fileNo));
+        Hashtable<String, ArffSaver> out = new Hashtable<String, ArffSaver>();
+
+        boolean eof = false;
+        ArffLoader.ArffReader instRead = new ArffLoader.ArffReader(inRead, this.header, 0, 0);
+        Instance inst;
+
+        while (!eof){
+
+            if ((inst = instRead.readInstance(this.header)) == null){
+                eof = true;
+                break;
+            }
+            ArffSaver writer;
+            String val = inst.stringValue(splitAttrib);
+
+            if ((writer = out.get(val)) == null){
+                
+                writer = new ArffSaver();
+                out.put(val, writer);
+
+                File file = new File(StringUtils.replace(this.output.get(fileNo), FileUtils.fileNameEncode(val)));
+
+                writer.setFile(file);
+                writer.setDestination(file);
+                writer.setStructure(this.header);
+                writer.setRetrieval(Saver.INCREMENTAL);
+            }
+            writer.writeIncremental(inst);
+        }
+
+        for (ArffSaver writer : out.values()){
+            writer.writeIncremental(null);
+        }
+
+        inRead.close();
+        inRead = null;
     }
 
 }
