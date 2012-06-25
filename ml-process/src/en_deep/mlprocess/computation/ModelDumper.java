7e2001de85403a5765394dde1db18f2a6213f7b5
tuetschek <tuetschek@gmail.com>
2012-06-25 15:32:58 +0000

    ModelDumper working with LibLINEAR

--- ml-process/src/en_deep/mlprocess/computation/ModelDumper.java
+++ ml-process/src/en_deep/mlprocess/computation/ModelDumper.java
@@ -16,17 +16,39 @@ import weka.classifiers.functions.LibLINEAR;
 import weka.core.Instances;
 
 /**
- *
- * @author odusek
+ * This is a helper class that dumps the trained model weights for LibLINEAR models.
+ * <p>
+ * It takes two input files (model, some compatible input data) per output file (feature weights dump),
+ * there is one possible parameter (<tt>threshold</tt> -- minimum absolute weight for a feature
+ * to be printed).
+ * </p>
+ * @author Ondrej Dusek
  */
 public class ModelDumper extends Task {
 
+    /* CONSTANTS */
+    
+    /** Name of the 'threshold' parameter */
+    private static final String THRESHOLD = "threshold";
+    
+    /* DATA */
+    
+    /** Minimum absolute value to be printed */
+    private double threshold = 0.0;
+    
+    
+    /* METHODS */
+    
     public ModelDumper(String id, Hashtable<String, String> parameters,
             Vector<String> input, Vector<String> output) throws TaskException {
         super(id, parameters, input, output);
         
         this.eliminatePatterns(this.input);
         this.eliminatePatterns(this.output);
+        
+        if (this.hasParameter(THRESHOLD)){
+            this.threshold = this.getDoubleParameterVal(THRESHOLD);
+        }
         
         if (this.input.size() != 2 * this.output.size()){
             throw new TaskException(TaskException.ERR_WRONG_NUM_OUTPUTS, this.id, "There must be twice as much inputs as outputs.");
@@ -56,25 +78,46 @@ public class ModelDumper extends Task {
 
         data.setClassIndex(model.classAttrib);       
         
-        // create copies of evaluation data for all models and prepare them
+        // prepare the model input data "as if" for classification (just to know the attribute labels)
         Instances modelInput = FileUtils.filterAttributes(data, model.selectedAttributes);
         modelInput.setClassIndex(model.attribsMask[model.classAttrib]);
 
-        // binarize, if supposed to
         if (model.binarize){
             Logger.getInstance().message(this.id + ": binarizing... (" + modelInput.relationName() + ")", Logger.V_DEBUG);
             modelInput = WekaClassifier.sparseNominalToBinary(modelInput);
         }
 
-        // LibLINEAR internal = (LibLINEAR) model.classif;
+        // retrieve the model weights
         liblinear.Model internal = (liblinear.Model) ((LibLINEAR) model.classif).getModel();
         double [] weights = internal.getFeatureWeights();
-        // String [] weights = internal.getWeights().split("\\s+");
         StringBuilder sb = new StringBuilder();
         
-        for (int i = 0; i < weights.length; ++i){
-            sb.append(modelInput.attribute(i)).append(" -- ").append(weights[i]).append("\n");            
+        int labelsNo = internal.getNrClass();
+        int featsNo = modelInput.numAttributes();
+        
+        int [] labelNums = internal.getLabels();
+            
+        // for each feature, print weights for all target classes
+        for (int feat = 0; feat < featsNo; ++feat){
+            for (int label = 0; label < labelsNo; ++label){
+                if (Math.abs(weights[feat * labelsNo + label]) < this.threshold){
+                    continue;
+                }
+                sb.append(modelInput.attribute(feat)).append(" / ").append(modelInput.classAttribute().value(labelNums[label]));
+                sb.append(" :\t").append(weights[feat * labelsNo + label]).append("\n");
+            }
         }
+        // do the same for the additional "BIAS" feature, if applicable
+        if (featsNo * labelsNo < weights.length){
+            for (int label = 0; label < labelsNo; ++label){
+                if (Math.abs(weights[featsNo * labelsNo + label]) < this.threshold){
+                    continue;
+                }
+                sb.append("BIAS / ").append(modelInput.classAttribute().value(labelNums[label]));
+                sb.append(" :\t").append(weights[featsNo * labelsNo + label]).append("\n");                
+            }
+        }
+        
         FileUtils.writeString(to, sb.toString());
     }
     
